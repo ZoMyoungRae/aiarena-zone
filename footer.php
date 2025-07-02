ca708387de4bb00aefe41bc49cdd30658c1e5396
Hidi <hizoko@naver.com>
2025-07-02 10:03:47 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer style="background-color: #f0f0f0; padding: 20px; text-align: center; font-size: 14px; color: #555; margin-top: 60px;">
+  <p>&copy; <?php echo date('Y'); ?> AllDe. All rights reserved.</p>
+  <p>📄 Powered by Hidi.Works x {z}</p>
+</footer>
+
+</body>
+</html>
